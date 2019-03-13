a6d68c47f81f7f8ac1909e58fc87967df62d6f36
ihargit <ihar.kharashun@gmail.com>
2019-03-13 16:16:42 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  let first, second, third, triangles;
+  triangles = 0;
+
+  for (i = 0; i < preferences.length - 1; i++) { 
+    first = i;
+
+    second = preferences[first] - 1; // -1 used to match persons position (item value) in the to its real position in the array 
+
+    third = preferences[second] - 1;
+
+    // there is an assumption that every person eather belong to the triangle or doesn't, so all already checked persons were excluded from further evaluation
+    if ((preferences[third] - 1) === first && third > first && third > second) {
+      triangles++;
+    }
+  }
+
+  return triangles;
 };
